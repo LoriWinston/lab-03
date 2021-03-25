c592c83c8c4fd076cc364762f7a632efbd671c45
Lori Winston <Lcwinston@gmail.com>
2021-03-24 17:58:04 -0700

    first working

--- demo/__tests__/app.test.js
+++ demo/__tests__/app.test.js
@@ -2,6 +2,8 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Order = require('../lib/models/Order');
+
 
 jest.mock('twilio', () => () => ({
   messages: {
@@ -14,6 +16,12 @@ describe('03_separation-of-concerns-demo routes', () => {
     return setup(pool);
   });
 
+  let order;
+  beforeEach(async() => {
+    order = await Order.insert({ quantity: 10 })
+  })
+
+
   it('creates a new order in our database and sends a text message', () => {
     return request(app)
       .post('/api/v1/orders')
@@ -21,7 +29,7 @@ describe('03_separation-of-concerns-demo routes', () => {
       .then((res) => {
         // expect(createMessage).toHaveBeenCalledTimes(1);
         expect(res.body).toEqual({
-          id: '1',
+          id: '2',
           quantity: 10,
         });
       });
@@ -33,8 +41,31 @@ describe('03_separation-of-concerns-demo routes', () => {
       .send({ quantity: 10 });
 
     expect(res.body).toEqual({
-      id: '1',
+      id: '2',
       quantity: 10,
     });
   });
-});
+
+
+// it('gets all the orders in our database', async () => {
+//     return request(app)
+//     .get('/api/v1/orders')
+//     .then((res) => {
+
+//       expect(res.body).toEqual([{
+//         id: '1',
+//         quantity: 10,
+//       }]);
+//     });
+// });
+
+it('ASYNC/AWAIT: retreives an order in our database', async () => {
+  const res = await request(app)
+      .get('/api/v1/orders')
+
+            expect(res.body[0]).toEqual({
+        id: '1',
+        quantity: 10,
+      });
+    });
+  });
